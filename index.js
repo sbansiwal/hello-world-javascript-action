95e0c61f4bea613ea9e2b3bbcd84591727ea430a
sunil-bansiwal <sunil.bansiwal@razorpay.com>
2020-05-16 23:55:27 +0530

    modified action.yml and index.js

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
   // console.log(`The event payload: ${payload}`);
-  if (nameToGreet == 'Sunil Bansiwal') {
+  const jiraToken = core.getInput('JIRA_API_TOKEN');
+  console.log(`Jira token value : ${jiraToken}`);
+
+  if (nameToGreet != 'Sunil Bansiwal') {
   	console.log(`The name is Sunil Bansiwal`);
   	core.setFailed(`Wrong person to greet`);
   } else {
